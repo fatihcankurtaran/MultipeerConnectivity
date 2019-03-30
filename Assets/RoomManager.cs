c24a8f7430ae552d8ae81441f69308f19e33514e
DESKTOP-C72EGO5\fthcn <fthcnkrtrn@gmail.com>
2019-03-30 23:16:03 +0300

    hh

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -5,17 +5,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Text;
+using NearbyDroids;
+using System.IO;
 
 public class RoomManager : MonoBehaviour
-{
-   
-    //IDiscoveryListener listener;
+
+{   public bool isClient
+    {
+        get;set;
+
+    }
+    NearbyRoom room;
+    bool connected;
+
+
+    private Action<NearbyRoom, bool> roomDiscoveredCallback;
+    
+    //private static RoomListener roomListener = new RoomListener();
+    // private readonly IDiscoveryListener listener;
     //  public InputField roomName, username;
     //private string localPlayerName;
     public const string RoomNameKey = "roomname";
     public const string PlayerNameKey = "playername";
     public GameObject  textObject;
-    IDiscoveryListener listener;
+    
     //private bool joining;
     void Awake()
     {
@@ -59,38 +72,46 @@ public class RoomManager : MonoBehaviour
     public void onCreateRoom()
     {
 
-
+        NearbyRoom.StopAll();
         PlayGamesPlatform.InitializeNearby((client) =>
         {
             Debug.Log("Nearby connections initialized");
         });
+        
+        NearbyRoom nearbyRoom = new NearbyRoom("Most Popular Room!!");
+        nearbyRoom.AutoJoin = true;
+        nearbyRoom.AlwaysOpen = true;
+        isClient = false; 
+        nearbyRoom.OpenRoom();
+        
 
-        List<string> appIdentifiers = new List<string>();
-        appIdentifiers.Add(PlayGamesPlatform.Nearby.GetAppBundleId());
-        PlayGamesPlatform.Nearby.StartAdvertising(
-                  "Awesome Game Host",  // User-friendly name
-                    appIdentifiers,  // App bundle Id for this game
-                   TimeSpan.FromSeconds(0),// 0 = advertise forever
-                   (AdvertisingResult result) =>
-                   {
 
-                       Debug.Log("OnAdvertisingResult: " + result);
-                   },
-                      (ConnectionRequest request) =>
-                      {
+        //List<string> appIdentifiers = new List<string>();
+        //appIdentifiers.Add(PlayGamesPlatform.Nearby.GetAppBundleId());
+        //PlayGamesPlatform.Nearby.StartAdvertising(
+        //          "Awesome Game Host",  // User-friendly name
+        //            appIdentifiers,  // App bundle Id for this game
+        //           TimeSpan.FromSeconds(0),// 0 = advertise forever
+        //           (AdvertisingResult result) =>
+        //           {
 
-                          Debug.Log("Received connection request: " +
-                           request.RemoteEndpoint.ServiceId + " " +
-                           request.RemoteEndpoint.EndpointId + " " +
-                           request.RemoteEndpoint.Name);
+        //               Debug.Log("OnAdvertisingResult: " + result);
+        //           },
+        //              (ConnectionRequest request) =>
+        //              {
 
-                      }
-                   );
+        //                  Debug.Log("Received connection request: " +
+        //                   request.RemoteEndpoint.ServiceId + " " +
+        //                   request.RemoteEndpoint.EndpointId + " " +
+        //                   request.RemoteEndpoint.Name);
+
+        //              }
+        //           );
 
     }
 
     byte[] bytes = Encoding.ASCII.GetBytes("asdasd");
-
+    
 
     public void OnEndpointFound(EndpointDetails discoveredEndpoint)
     {
@@ -111,27 +132,77 @@ public class RoomManager : MonoBehaviour
     }
     public void StartMultiplayer()
     {
-        
-        
+
        
+
 
     }
     
-
-
-    internal void StartDiscovery()
+    public void StartDiscovery()
     {
+
+        isClient = true;
+        Debug.Log("StartDiscovery called");
+        onStartDiscovery();
+    }
+
+
+    internal void onStartDiscovery()
+    {
+
+        NearbyRoom.StopAll();
+
+
+        NearbyRoom.FindRooms(OnRoomFound); 
         
-
-        PlayGamesPlatform.Nearby.StartDiscovery(
-                        PlayGamesPlatform.Nearby.GetServiceId(),
-                        TimeSpan.FromSeconds(0),
-                        listener);
+        //PlayGamesPlatform.InitializeNearby((client) =>
+        //{
+        //    Debug.Log("Nearby connections initialized");
+        //});
+        //Debug.Log("onStartDiscovery called"); 
+        //PlayGamesPlatform.Nearby.StartDiscovery(
+        //                PlayGamesPlatform.Nearby.GetServiceId(),
+        //                TimeSpan.FromSeconds(0),
+        //                listener);
     }
-
-
-    public void OnEndpointLost(string lostEndpointId)
+      
+    internal void OnRoomFound(NearbyRoom room, bool available)
     {
-        Debug.Log("Endpoint lost: " + lostEndpointId);
+
+        if(available)
+
+        {
+
+            NearbyPlayer nearbyPlayer = new NearbyPlayer("Fatih");
+            
+            room.JoinRoom(nearbyPlayer, Encoding.ASCII.GetBytes("asdasd"), OnRoomJoined);
+            // GameObject obj = Instantiate() as GameObject;
+        }
     }
+    
+    internal void OnRoomJoined (ConnectionResponse response)
+    {
+        Debug.Log("OnRoomJoined Called status: " + response.ResponseStatus);
+        if (response.ResponseStatus == ConnectionResponse.Status.Accepted)
+        {
+            // if we are connected, stop looking for rooms.
+            NearbyRoom.StopRoomDiscovery();
+
+            // the first payload is sent with the response so we can initialize
+            // the game scene.
+            OnMessageReceived(room.Address, response.Payload);
+            connected = true;
+        }
+    }
+    internal void OnMessageReceived(NearbyPlayer sender, byte[] data)
+    {
+        UpdateGameStateFromData(data);
+    }
+    internal void UpdateGameStateFromData(byte[] data)
+    {
+        MemoryStream ms = new MemoryStream(data);
+      
+        
+    }
+
 }
--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -9,6 +9,10 @@ public class SceneController : MonoBehaviour {
 
     public void ChangeScene(int sceneId )
     {
+
+        
         SceneManager.LoadScene(sceneId);
+
+        
     }
 }
